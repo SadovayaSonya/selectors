86116ce3cb30e64e2c440fb608509754fca27c59
daniildruzhinin <ddv_2010@mail.ru>
2023-02-06 20:26:50 +0200

    add selectors

--- tests/check_selectors.test.js
+++ tests/check_selectors.test.js
@@ -2,8 +2,17 @@ Feature('Selectors');
 
 Scenario('test any selectors',  ({ I }) => {
     I.amOnPage('/');
-    I.waitForVisible(locate('[class = transitional-main__event-button]').at(1));
-    I.click('[class = transitional-main__event-button]');
     pause();
+
+    I.waitForVisible(locate('[class*="nav__scroll course-categories__nav-box"]').first());
+    // I.click('//div[@class="button button_white button_white-inverse-transparent button_md-3 button_radius-md None"]');
+    I.click('div[class*="transitional-main"] div[class*="button_white-inverse-transparent"]');
+    // I.waitForClickable('//button[@class="header3__button-sign-in"]');
+    // I.click('//button[@class="header3__button-sign-in"]');
+    // I.click('//*[@data-tab-id="register"]');
+    // I.fillField('input[name=fname][class*=new-input][class*=new-input_full]', 'testUsername');
+    // I.fillField('form[class*="registration"] input[type="email"]', 'testEmail');
+    // I.fillField('/html/body/div[2]/div/div/div/div[3]/div[3]/form/div[2]/div[1]/div[3]/div/input', 'testSecondName');
+
 
 });
